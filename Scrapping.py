12eaeca58f166fc994052ead599e38cb8a84f33a
Sheril <sherilmariamjose@gmail.com>
2022-12-17 20:01:57 +0530

    Second commit manifest of chrome extension and web scrapping for links and pdf files.

--- Scrapping.py
+++ Scrapping.py
@@ -1,4 +1,4 @@
-#web scraping
+#web scraping and extracting pdf files
 import requests
 import ssl
 ssl._create_default_https_context=ssl._create_unverified_context
@@ -10,12 +10,13 @@ read=requests.get(url)
 html_content=read.content
 soup=BeautifulSoup(html_content,"html.parser")
 list_of_pdf=set()
-l=soup.find('p')
-p=l.find_all('a')
-for link in p:
-     print("links: ", link.get('href'))
-     print("\n")
-     pdf_link = (link.get('href')[:-5]) + ".pdf"
-     print(pdf_link)
-     print("\n")
-     list_of_pdf.add(pdf_link)
+l=soup.find_all('p')
+for pe in l:
+    p=pe.find_all('a')
+    for link in p:
+         print("links: ", link.get('href'))
+         print("\n")
+         pdf_link = (link.get('href')[:-5]) + ".pdf"
+         print(pdf_link)
+         print("\n")
+         list_of_pdf.add(pdf_link)
